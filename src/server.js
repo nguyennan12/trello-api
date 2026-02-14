d85c78a17dcbaa3da2e9e9a031e5088a71e07b5e
nguyenan12 <nguyenann1204@gmail.com>
2026-02-14 21:03:58 +0700

    update env for project

--- src/server.js
+++ src/server.js
@@ -1,24 +1,38 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    // Test Absolute import mapOrder
+    //console.log(await GET_DB().listCollections().toArray())
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}`)
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at http://${hostname}:${port}`)
-})
+  })
+
+  exitHook(() => {
+    console.log('Exitting')
+    CLOSE_DB()
+  })
+}
+
+CONNECT_DB()
+  .then(() => console.log('connect mongoDb success'))
+  .then(() => START_SERVER())
+  .catch(error => {
+    console.error(error)
+    process.exit(0)
+  })
+
+
